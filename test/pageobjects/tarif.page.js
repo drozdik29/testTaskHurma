477e81c8ffd82638bedbb872dc37dccb0b0690e7
Max Drozdovsky <drozdovskiimax@gmail.com>
2023-11-25 14:57:46 +0200

    Виконане тестове завдання

--- test/pageobjects/tarif.page.js
+++ test/pageobjects/tarif.page.js
@@ -15,10 +15,10 @@ class TarPage extends Page {
         return $('//*[@id="tariff-page"]/div[1]/div[2]/div[2]/div[2]/div[1]/span[2]')
     }
 
-
     get oneMonthBtn() {
         return $('//*[@id="tariff-page"]/div[1]/div[1]/div[1]/div[1]');
     }
+
     get threeMonthBtn() {
         return $('//*[@id="tariff-page"]/div[1]/div[1]/div[1]/div[2]')
     }
@@ -38,6 +38,70 @@ class TarPage extends Page {
         return $('//*[@id="tariff-page"]/div[1]/div[2]/div[2]/div[3]/div/div/input')
     }
 
+    get recrFormRecrTMoreBtn(){
+        return $('//*[@id="tariff-page"]/div[1]/div[2]/div[2]/div[3]/div/div/span[1]')
+    }
+
+    get recrFormAllTMoreBtn(){
+        return $('//*[@id="tariff-page"]/div[1]/div[2]/div[1]/div[3]/div[2]/div/span[1]')
+    }
+
+    get employeesFormAll(){
+        return $('//*[@id="tariff-page"]/div[1]/div[2]/div[1]/div[3]/div[1]/div/select')
+    }
+
+    get employees31All(){
+        return $('//*[@id="tariff-page"]/div[1]/div[2]/div[1]/div[3]/div[1]/div/select/option[2]')
+    }
+
+    get employees51All(){
+        return $('//*[@id="tariff-page"]/div[1]/div[2]/div[1]/div[3]/div[1]/div/select/option[3]')
+    }
+
+    get employees101All(){
+        return $('//*[@id="tariff-page"]/div[1]/div[2]/div[1]/div[3]/div[1]/div/select/option[4]')
+    }
+
+    get employees151All(){
+        return $('//*[@id="tariff-page"]/div[1]/div[2]/div[1]/div[3]/div[1]/div/select/option[5]')
+    }
+
+    get employees201All(){
+        return $('//*[@id="tariff-page"]/div[1]/div[2]/div[1]/div[3]/div[1]/div/select/option[6]')
+    }
+
+    get employees251All(){
+        return $('//*[@id="tariff-page"]/div[1]/div[2]/div[1]/div[3]/div[1]/div/select/option[7]')
+    }
+
+    get employeesFormHr(){
+        return $('//*[@id="tariff-page"]/div[1]/div[2]/div[3]/div[3]/div/div/select')
+    }
+
+    get employees31Hr(){
+        return $('//*[@id="tariff-page"]/div[1]/div[2]/div[3]/div[3]/div/div/select/option[2]')
+    }
+
+    get employees51Hr(){
+        return $('//*[@id="tariff-page"]/div[1]/div[2]/div[3]/div[3]/div/div/select/option[3]')
+    }
+
+    get employees101Hr(){
+        return $('//*[@id="tariff-page"]/div[1]/div[2]/div[3]/div[3]/div/div/select/option[4]')
+    }
+
+    get employees151Hr(){
+        return $('//*[@id="tariff-page"]/div[1]/div[2]/div[3]/div[3]/div/div/select/option[5]')
+    }
+
+    get employees201Hr(){
+        return $('//*[@id="tariff-page"]/div[1]/div[2]/div[3]/div[3]/div/div/select/option[6]')
+    }
+
+    get employees251Hr(){
+        return $('//*[@id="tariff-page"]/div[1]/div[2]/div[3]/div[3]/div/div/select/option[7]')
+    }
+    
     open() {
         return super.open('');
     }
--- test/specs/test.e2e.js
+++ test/specs/test.e2e.js
@@ -8,59 +8,115 @@ describe('Test task for Hurma', () => {
     });
 
     it('should show how bigger will be price', async () => {
-        const all3 = await TarPage.allInclusivePrice.getText()
-        const rec3 = await TarPage.recruitmentPrice.getText()
-            ; await TarPage.hrPrice.getText()
+        const all3 = await TarPage.allInclusivePrice.getText();
+        const rec3 = await TarPage.recruitmentPrice.getText();
+        const hr3 = await TarPage.hrPrice.getText();
 
-                (await TarPage.oneMonthBtn).click()
+        (await TarPage.oneMonthBtn).click();
         await browser.pause(2000);
-        const all1 = await TarPage.allInclusivePrice.getText()
-        const rec1 = await TarPage.recruitmentPrice.getText()
-        const hr1 = await TarPage.hrPrice.getText()
-            (TarPage.sixMonthBtn).click()
+        const all1 = await TarPage.allInclusivePrice.getText();
+        const rec1 = await TarPage.recruitmentPrice.getText();
+        const hr1 = await TarPage.hrPrice.getText();
+        (TarPage.sixMonthBtn).click();
         await browser.pause(2000);
-        const all6 = await TarPage.allInclusivePrice.getText()
-        const rec6 = await TarPage.recruitmentPrice.getText()
-        const hr6 = await TarPage.hrPrice.getText()
+        const all6 = await TarPage.allInclusivePrice.getText();
+        const rec6 = await TarPage.recruitmentPrice.getText();
+        const hr6 = await TarPage.hrPrice.getText();
 
-            (TarPage.twelveMonthBtn).click()
+        (TarPage.twelveMonthBtn).click();
         await browser.pause(2000);
-        const all12 = await TarPage.allInclusivePrice.getText()
-        const rec12 = await TarPage.recruitmentPrice.getText()
-        const hr12 = await TarPage.hrPrice.getText()
+        const all12 = await TarPage.allInclusivePrice.getText();
+        const rec12 = await TarPage.recruitmentPrice.getText();
+        const hr12 = await TarPage.hrPrice.getText();
 
-        expect(all12).toBeLessThan(all6);
-        expect(all6).toBeLessThan(all3);
-        expect(all3).toBeLessThan(all1);
+        expect(parseFloat(all12)).toBeLessThan(parseFloat(all6));
+        expect(parseFloat(all6)).toBeLessThan(parseFloat(all3));
+        expect(parseFloat(all3)).toBeLessThan(parseFloat(all1));
 
-        expect(hr12).toBeLessThan(hr6);
-        expect(hr6).toBeLessThan(hr3);
-        expect(hr3).toBeLessThan(hr1);
+        expect(parseFloat(hr12)).toBeLessThan(parseFloat(hr6));
+        expect(parseFloat(hr6)).toBeLessThan(parseFloat(hr3));
+        expect(parseFloat(hr3)).toBeLessThan(parseFloat(hr1));
 
-        expect(rec12).toBeLessThan(rec6);
-        expect(rec6).toBeLessThan(rec3);
-        expect(rec3).toBeLessThan(rec1);
+        expect(parseFloat(rec12)).toBeLessThan(parseFloat(rec6));
+        expect(parseFloat(rec6)).toBeLessThan(parseFloat(rec3));
+        expect(parseFloat(rec3)).toBeLessThan(parseFloat(rec1));
+
 
     })
     it('should show how bigger will be price with more recruiters in All Tariff', async () => {
-        await TarPage.oneMonthBtn.click()
-        await TarPage.recrFormAll.clearValue()
-        await TarPage.recrFormAll.setValue(2)
-        const twoRec = await TarPage.allInclusivePrice.getText()
-            (await TarPage.recrFormAll).setValue(3)
-        const threeRec = await TarPage.allInclusivePrice.getText()
-            (await TarPage.recrFormAll).setValue(5)
-        const fiveRec = await TarPage.allInclusivePrice.getText()
+        await TarPage.oneMonthBtn.click();
+        const oneRec = parseFloat(await TarPage.allInclusivePrice.getText());
+        console.log(oneRec);
+        browser.pause(1000);
+        (await TarPage.recrFormAllTMoreBtn).click();
+        const twoRec = parseFloat(await TarPage.allInclusivePrice.getText());
+        console.log(twoRec);
+        (await TarPage.recrFormAllTMoreBtn).click();
+        const threeRec = parseFloat(await TarPage.allInclusivePrice.getText());
+        (await TarPage.recrFormAllTMoreBtn).click();
+        (await TarPage.recrFormAllTMoreBtn).click();
+        const fiveRec = parseFloat(await TarPage.allInclusivePrice.getText());
+        expect(parseFloat(twoRec) - parseFloat(oneRec)).toBeGreaterThanOrEqual(45);
+        expect(parseFloat(threeRec) - parseFloat(oneRec)).toBeGreaterThanOrEqual(90);
+        expect(parseFloat(fiveRec) - parseFloat(oneRec)).toBeGreaterThanOrEqual(180);
     })
 
     it('should show how bigger will be price with more recruiters in Recruitment Tariff', async () => {
-        (await TarPage.recrFormRecr).clearValue()
-        await TarPage.recrFormAll.setValue(2)
-        const twoRecRecT = await TarPage.recruitmentPrice.getText()
-            (await TarPage.recrFormAll).setValue(3)
-        const threeRecRecT = await TarPage.recruitmentPrice.getText()
-            (await TarPage.recrFormAll).setValue(5)
-        const fiveRecRecT = await TarPage.recruitmentPrice.getText()
+        await TarPage.recrFormRecrTMoreBtn.click();
+        const twoRecRecT = parseFloat(await TarPage.recruitmentPrice.getText());
+        await TarPage.recrFormRecrTMoreBtn.click();
+        const threeRecRecT = parseFloat(await TarPage.recruitmentPrice.getText());
+        await TarPage.recrFormRecrTMoreBtn.click();
+        await TarPage.recrFormRecrTMoreBtn.click();
+        const fiveRecRecT = parseFloat(await TarPage.recruitmentPrice.getText());
+        expect(parseFloat(fiveRecRecT) - parseFloat(threeRecRecT)).toBeGreaterThanOrEqual(90);
+        expect(parseFloat(fiveRecRecT) - parseFloat(twoRecRecT)).toBeGreaterThanOrEqual(135);
+    })
+
+    it('should show how bigger will be price with more recruiters with more employees in All Tariff', async () => {
+        const price1 = parseFloat(await TarPage.allInclusivePrice.getText());
+        (await TarPage.employees31All).click();
+        const price31 = parseFloat(await TarPage.allInclusivePrice.getText());
+        (await TarPage.employees51All).click();
+        const price51 = parseFloat(await TarPage.allInclusivePrice.getText());
+        (await TarPage.employees101All).click();
+        const price101 = parseFloat(await TarPage.allInclusivePrice.getText());
+        (await TarPage.employees151All).click();
+        const price151 = parseFloat(await TarPage.allInclusivePrice.getText());
+        (await TarPage.employees201All).click();
+        const price201 = parseFloat(await TarPage.allInclusivePrice.getText());
+        (await TarPage.employees251All).click();
+        const price251 = parseFloat(await TarPage.allInclusivePrice.getText());
+
+        expect(parseFloat(price31) - parseFloat(price1)).toBeGreaterThanOrEqual(60);
+        expect(parseFloat(price51) - parseFloat(price31)).toBeGreaterThanOrEqual(70);
+        expect(parseFloat(price101) - parseFloat(price51)).toBeGreaterThanOrEqual(70);
+        expect(parseFloat(price151) - parseFloat(price101)).toBeGreaterThanOrEqual(30);
+        expect(parseFloat(price201) - parseFloat(price151)).toBeGreaterThanOrEqual(75);
+        expect(parseFloat(price251) - parseFloat(price201)).toBeGreaterThanOrEqual(75);
+    })
+
+    it('should show how bigger will be price with more recruiters with more employees in HR Tariff', async () => {
+        const price1 = parseFloat(await TarPage.hrPrice.getText());
+        (await TarPage.employees31Hr).click();
+        const price31 = parseFloat(await TarPage.hrPrice.getText());
+        (await TarPage.employees51Hr).click();
+        const price51 = parseFloat(await TarPage.hrPrice.getText());
+        (await TarPage.employees101Hr).click();
+        const price101 = parseFloat(await TarPage.hrPrice.getText());
+        (await TarPage.employees151Hr).click();
+        const price151 = parseFloat(await TarPage.hrPrice.getText());
+        (await TarPage.employees201Hr).click();
+        const price201 = parseFloat(await TarPage.hrPrice.getText());
+        (await TarPage.employees251Hr).click();
+        const price251 = parseFloat(await TarPage.hrPrice.getText());
+
+        expect(parseFloat(price31) - parseFloat(price1)).toBeGreaterThanOrEqual(60);
+        expect(parseFloat(price51) - parseFloat(price31)).toBeGreaterThanOrEqual(70);
+        expect(parseFloat(price101) - parseFloat(price51)).toBeGreaterThanOrEqual(70);
+        expect(parseFloat(price151) - parseFloat(price101)).toBeGreaterThanOrEqual(30);
+        expect(parseFloat(price201) - parseFloat(price151)).toBeGreaterThanOrEqual(75);
+        expect(parseFloat(price251) - parseFloat(price201)).toBeGreaterThanOrEqual(75);
     })
 })
 
